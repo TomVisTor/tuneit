0b5f4a737b728a77c1518cf2f94345f79c7e8b56
Thomas Visset <thmvisset31@gmail.com>
2021-03-01 17:58:37 +0100

    Add models User Instrument Reservation

---
+++ db/migrate/20210301165758_create_reservations.rb
@@ -0,0 +1,11 @@
+class CreateReservations < ActiveRecord::Migration[6.0]
+  def change
+    create_table :reservations do |t|
+      t.integer :total_price
+      t.date :start_date
+      t.date :end_date
+
+      t.timestamps
+    end
+  end
+end
