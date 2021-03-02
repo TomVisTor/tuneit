97e59fd5f143b87acdd717658b58d4375f7ccb4a
Miebi Iyeyemi <iyeyemim@gmail.com>
2021-03-02 12:17:07 +0100

    Add validations

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -1,4 +1,9 @@
 class Instrument < ApplicationRecord
   belongs_to :owner, class_name: "User"
   has_many :reservations
+
+  validates :name, presence: true
+  validates :family, presence: true
+  validates :price, presence: true
+  validates :description, presence: true
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,8 @@
 class Reservation < ApplicationRecord
   belongs_to :renter, class_name: "User"
   belongs_to :instrument
+
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :total_price, presence: true
 end
