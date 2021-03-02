bdc0aaac2a3544afc6658d1f55db6fc4a4a33f5d
Miebi Iyeyemi <iyeyemim@gmail.com>
2021-03-02 12:42:13 +0100

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,23 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # As renter
+  resources :instruments, only: [ :index, :show ] do
+    resources :reservations, only: [ :create ]
+  end
+
+  resources :reservations, only: [ :index ]
+
+  # As owner
+  namespace :owner do
+    resources :reservations, only: [ :index ] do
+      member do
+        patch :accept
+        patch :decline
+      end
+    end
+
+    resources :instruments, only: [ :index, :new, :create ]
+  end
 end
