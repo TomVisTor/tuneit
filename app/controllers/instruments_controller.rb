4690f72fd26bf2b7c598f843899c0de850de0eb7
Miebi Iyeyemi <iyeyemim@gmail.com>
2021-03-02 22:37:36 +0100

    Initialize show page

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -1,7 +1,19 @@
 class InstrumentsController < ApplicationController
+  before_action :set_instrument, only: [:show]
+
   def index
   end
 
   def show
+  end
+
+  private
+
+  def set_instrument
+    @instrument = Instrument.find(params[:id])
+  end
+
+  def instrument_params
+    params.require(:instrument).permit(:name, :family, :availability, :description, :photo, :price_per_day, :owner_id)
   end
 end
