d6eac12f12b26f0cc6c0eb44162d19f0d379aefd
francois xavier paris <fxavierparis@gmail.com>
2021-03-05 11:25:21 +0100

    edit et destroy actifs

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,6 @@ Rails.application.routes.draw do
       end
     end
 
-    resources :instruments, only: [ :index, :new, :create, :destroy ]
+    resources :instruments, only: [ :index, :new, :create, :destroy, :edit, :update ]
   end
 end
